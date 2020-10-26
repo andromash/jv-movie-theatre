a356b9da530928275646441870de6598c13aa163
ROMASHSX <andriiromash@gmail.com>
2020-10-26 13:22:15 +0200

    cinemahall controller and dto created

---
+++ src/main/java/com/hibernate/cinema/controller/CinemaHallController.java
@@ -0,0 +1,39 @@
+package com.hibernate.cinema.controller;
+
+import com.hibernate.cinema.model.dto.CinemaHallRequestDto;
+import com.hibernate.cinema.model.dto.CinemaHallResponseDto;
+import com.hibernate.cinema.service.CinemaHallService;
+import com.hibernate.cinema.service.mapper.CinemaHallMapper;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cinema-halls")
+public class CinemaHallController {
+    private final CinemaHallService cinemaHallService;
+    private final CinemaHallMapper cinemaHallMapper;
+
+    @Autowired
+    public CinemaHallController(CinemaHallService cinemaHallService, CinemaHallMapper cinemaHallMapper) {
+        this.cinemaHallService = cinemaHallService;
+        this.cinemaHallMapper = cinemaHallMapper;
+    }
+
+    @PostMapping
+    public void addCinemaHall(@RequestBody CinemaHallRequestDto cinemaHall) {
+        cinemaHallService.add(cinemaHallMapper.castDtoToCinemaHall(cinemaHall));
+    }
+
+    @GetMapping
+    public List<CinemaHallResponseDto> getAllCinemaHalls() {
+        return cinemaHallService.getAll().stream()
+                .map(cinemaHallMapper::castCinemaHallToDto)
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/com/hibernate/cinema/model/dto/CinemaHallResponseDto.java
@@ -0,0 +1,31 @@
+package com.hibernate.cinema.model.dto;
+
+public class CinemaHallResponseDto {
+    private long id;
+    private int capacity;
+    private String description;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
