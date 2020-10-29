d826c7b7f5e0dc71b2a4f2d0d59382ce88bea986
ROMASHSX <andriiromash@gmail.com>
2020-10-29 16:07:41 +0200

    checkstyle done

--- src/main/java/com/hibernate/cinema/controller/MovieController.java
+++ src/main/java/com/hibernate/cinema/controller/MovieController.java
@@ -6,14 +6,13 @@ import com.hibernate.cinema.service.MovieService;
 import com.hibernate.cinema.service.mapper.MovieMapper;
 import java.util.List;
 import java.util.stream.Collectors;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/movies")
--- src/main/java/com/hibernate/cinema/controller/MovieSessionController.java
+++ src/main/java/com/hibernate/cinema/controller/MovieSessionController.java
@@ -7,6 +7,7 @@ import com.hibernate.cinema.service.mapper.MovieSessionMapper;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,8 +16,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/movie-sessions")
