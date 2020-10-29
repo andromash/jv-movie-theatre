c9aff10924b3e8eede8d9e05463aadd8a0dbcb6d
ROMASHSX <andriiromash@gmail.com>
2020-10-29 15:44:55 +0200

    user controller is changed

--- src/main/java/com/hibernate/cinema/controller/UserController.java
+++ src/main/java/com/hibernate/cinema/controller/UserController.java
@@ -4,9 +4,9 @@ import com.hibernate.cinema.model.dto.UserResponseDto;
 import com.hibernate.cinema.service.UserService;
 import com.hibernate.cinema.service.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,7 +22,7 @@ public class UserController {
     }
 
     @GetMapping("/by-email")
-    public UserResponseDto getUserByEmail(Authentication user) {
-        return userMapper.mapUserToDto(userService.findByEmail(user.getName()));
+    public UserResponseDto getUserByEmail(@RequestParam String email) {
+        return userMapper.mapUserToDto(userService.findByEmail(email));
     }
 }
