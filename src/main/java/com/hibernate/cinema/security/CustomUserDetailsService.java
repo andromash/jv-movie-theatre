28f6ed8782b9f0b7b101b25272e35480362c2429
ROMASHSX <andriiromash@gmail.com>
2020-11-03 18:04:00 +0200

    UserBuilder is changed

--- src/main/java/com/hibernate/cinema/security/CustomUserDetailsService.java
+++ src/main/java/com/hibernate/cinema/security/CustomUserDetailsService.java
@@ -2,14 +2,13 @@ package com.hibernate.cinema.security;
 
 import com.hibernate.cinema.dao.UserDao;
 import com.hibernate.cinema.model.User;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
