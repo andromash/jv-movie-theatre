a137cfb9a535576c0c07dd7d96a44f94613ec30b
ROMASHSX <andriiromash@gmail.com>
2020-11-03 18:01:22 +0200

    UserBuilder is changed

--- src/main/java/com/hibernate/cinema/security/CustomUserDetailsService.java
+++ src/main/java/com/hibernate/cinema/security/CustomUserDetailsService.java
@@ -1,7 +1,7 @@
 package com.hibernate.cinema.security;
 
+import com.hibernate.cinema.dao.UserDao;
 import com.hibernate.cinema.model.User;
-import com.hibernate.cinema.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -9,30 +9,28 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
-    private final UserService userService;
+    private final UserDao userDao;
 
     @Autowired
-    public CustomUserDetailsService(UserService userService) {
-        this.userService = userService;
+    public CustomUserDetailsService(UserDao userDao) {
+        this.userDao = userDao;
     }
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        User user = userService.findByEmail(email);
-        UserBuilder builder = null;
-        if (user != null) {
-            builder = org.springframework.security.core.userdetails.User.withUsername(email);
+        Optional<User> optionalUser = userDao.findByEmail(email);
+        User user = optionalUser.orElseThrow(() -> new UsernameNotFoundException(
+                "User with email " + email + "not found."));
+        UserBuilder builder = org.springframework.security.core.userdetails.User.withUsername(email);
             builder.password(user.getPassword());
             String[] roles = user.getRoles().stream()
                     .map(role -> role.getRoleName().toString())
                     .toArray(String[]::new);
             builder.roles(roles);
-        } else {
-            throw new UsernameNotFoundException("User with email " + email + "not found.");
-        }
-
         return builder.build();
     }
 }
